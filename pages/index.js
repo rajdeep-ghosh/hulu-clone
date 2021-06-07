7c0050a7e3cde8baa3397966f5a89813461b028c
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-07 11:10:51 +0530

    Add meta description

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ export default function Home() {
     <div>
       <Head>
         <title>Hulu Clone</title>
+        <meta
+          name="description"
+          content="Hulu Clone built using Next.js and Tailwind CSS"
+        />
       </Head>
 
       <h1>Lets build Hulu</h1>
