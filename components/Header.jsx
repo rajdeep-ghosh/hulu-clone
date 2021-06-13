abe608034bfa4f7723d34a6dbe57a8f946b5a554
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-13 20:12:03 +0530

    Add header icons

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 function Header() {
   return (
     <div>
-      <h1>This is the Header</h1>
       <Image src="/hulu-logo.png" width={200} height={100} />
     </div>
   );
