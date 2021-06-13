39628342aaca6e789e37f1912cf370f8816269cc
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-13 21:15:15 +0530

    Complete Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,11 +11,21 @@ import {
 
 function Header() {
   return (
-    <div>
-      <div>
-        <HeaderItem title="HOME" Icon={HomeIcon} />
+    <div className="m-5 flex flex-col sm:flex-row justify-between items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="home" Icon={HomeIcon} />
+        <HeaderItem title="trending" Icon={LightningBoltIcon} />
+        <HeaderItem title="verified" Icon={BadgeCheckIcon} />
+        <HeaderItem title="collections" Icon={CollectionIcon} />
+        <HeaderItem title="search" Icon={SearchIcon} />
+        <HeaderItem title="account" Icon={UserIcon} />
       </div>
-      <Image src="/hulu-logo.png" width={200} height={100} />
+      <Image 
+        className="order-first sm:order-none" 
+        src="/hulu-logo.png" 
+        width={200} 
+        height={100}        
+      />
     </div>
   );
 }
--- components/HeaderItem.jsx
+++ components/HeaderItem.jsx
@@ -1,9 +1,9 @@
 function HeaderItem({ title, Icon }) {
   return (
     <div className="flex flex-col items-center group w-12 sm:w-20 hover:text-white cursor-pointer">
-      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
-      <p className="tracking-widest opacity-100 group-hover:opacity-100">
-        {title}      
+      <Icon className=" h-7 mb-1 group-hover:animate-bounce" />
+      <p className="tracking-widest uppercase opacity-0 group-hover:opacity-100">
+        {title}
       </p>
     </div>
   );
