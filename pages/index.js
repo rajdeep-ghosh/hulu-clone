0d0eea6a469e7d9ddc26e0c0b14a387cf94f1e2e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-13 19:24:23 +0530

    Create Header component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Hulu Clone</title>
         <meta
           name="description"
           content="Hulu Clone built using Next.js and Tailwind CSS"
         />
+        <title>Hulu Clone</title>
       </Head>
 
-      <h1>Lets build Hulu</h1>
+      <Header />
     </div>
   );
 }
