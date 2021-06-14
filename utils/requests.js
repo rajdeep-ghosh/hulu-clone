d3425bd97e10d6ef1dde7ce2a211c9093e5d8ea9
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-14 15:43:31 +0530

    Complete requests.js

--- utils/requests.js
+++ utils/requests.js
@@ -9,13 +9,21 @@ export default {
     title: "Top Rated",
     url: `/movie/top_rated?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc`,
   },
-  fetchActionMovies: {
-    title: "Action",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=28&sort_by=popularity.desc`,
+  fetchTV: {
+    title: "TV",
+    url: `/tv/top_rated?api_key=${API_KEY}&language=en-US`,
   },
   fetchAnimatedMovies: {
     title: "Animation",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=16&sort_by=popularity.desc`,
+  },
+  fetchAnime: {
+    title: "Anime",
+    url: `/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&with_genres=16&with_original_language=ja&with_watch_monetization_types=flatrate`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28&sort_by=popularity.desc`,
   },
   fetchAnimeMovies: {
     title: "Anime Movies",
@@ -25,16 +33,24 @@ export default {
     title: "Sci Fi",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=878&sort_by=popularity.desc`,
   },
+  fetchWebSeries: {
+    title: "Series",
+    url: `/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&include_null_first_air_dates=false&with_watch_monetization_types=flatrate`,
+  },
   fetchComedyMovies: {
     title: "Comedy",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=35&sort_by=popularity.desc`,
   },
-  fetchAnime: {
-    title: "Anime",
-    url: `/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&with_genres=16&with_original_language=ja&with_watch_monetization_types=flatrate`,
+  fetchRomanceMovies: {
+    title: "Romance",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749&sort_by=popularity.desc`,
   },
-  fetchWebSeries: {
-    title: "Series",
-    url: `/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&include_null_first_air_dates=false&with_watch_monetization_types=flatrate`,
+  fetchHorrorMovies: {
+    title: "Horror",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27&sort_by=popularity.desc`,
+  },
+  fetchDocumantaries: {
+    title: "Documentaries",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=99&sort_by=popularity.desc`,
   },
 };
