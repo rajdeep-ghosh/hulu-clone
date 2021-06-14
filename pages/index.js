6782057582162234864eb17369f954ee504520db
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-15 01:25:00 +0530

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ export default function Home() {
           content="Hulu Clone built using Next.js and Tailwind CSS"
         />
         <title>Hulu Clone</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest"></link>
       </Head>
 
       <Header />
