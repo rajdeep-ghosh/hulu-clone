893387b91857b95117e5bd6c05e5170383a71f28
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-16 18:56:33 +0530

    Refactor Navbar last child

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -11,7 +11,7 @@ function Nav() {
           return (
             <h2 
               key={key} 
-              className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 last:pr-6"
+              className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 last:pr-8 sm:last:pr-20"
             >
               {title}
             </h2>
