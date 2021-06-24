c568c6353017b48ce14c784611085e80f57cc1fd
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-25 03:03:03 +0530

    Refactor

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="flex flex-col justify-center items-center p-2">
       <Image src="/tmdb.svg" width={128} height={128} />
-      <p className="text-lg font-medium">
+      <p className=" mb-2 text-lg font-medium">
         &copy; Rajdeep Ghosh | {" "} 
         <a href="https://github.com/rajdeep-ghosh/" target="_blank">Github | </a>
         <a href="https://linkedin.com/in/rajdeepghosh01/" target="_blank">LinkedIn</a>
