9b89a125164a19ec7b23f71279cca7d1ff8b072e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-25 02:03:10 +0530

    Style Details

--- components/Details.jsx
+++ components/Details.jsx
@@ -2,13 +2,15 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 
 function Details({ result }) {
   return (
-    <div>
-      <p>{result.overview}</p>
-      <h2>{result.title || result.original_title || result.name || result.original_name}</h2>
-      <p>
+    <div className="p-2">
+      <p className="truncate">{result.overview}</p>
+      <h2 className="mt-1 text-2xl text-white transition-all ease-in-out duration-100 group-hover:font-bold">
+        {result.title || result.original_title || result.name || result.original_name}
+      </h2>
+      <p className="flex items-center opacity-0 group-hover:opacity-100 capitalize">
         {result.media_type && `${result.media_type} · `}{" "}
-        {result.release_date || result.first_air_date} ·{" "}
-        <ThumbUpIcon className="h-5 inline" /> {" "}
+        {result.release_date || result.first_air_date} ·
+        <ThumbUpIcon className="h-5 mx-1" /> {" "}
         {result.vote_count}
       </p>
     </div>
--- components/Main.jsx
+++ components/Main.jsx
@@ -7,7 +7,7 @@ function Main({ results }) {
     <div>
       {results.map((result) => {
         return (
-          <div>
+          <div className="group p-2 cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Thumbnail backdrop={result.backdrop_path} poster={result.poster_path} />
             <Details result={result} />
           </div>
