5517feddff242a36710b5d3ab423f1aa92b9e417
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-25 02:29:28 +0530

    Refactor

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,12 +2,14 @@ import Thumbnail from "./Thumbnail";
 import Details from "./Details";
 
 function Main({ results }) {
-  console.log(results);
   return (
     <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3">
       {results.map((result) => {
         return (
-          <div className="group p-2 cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+          <div 
+            key={result.id}
+            className="group p-2 cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+          >
             <Thumbnail backdrop={result.backdrop_path} poster={result.poster_path} />
             <Details result={result} />
           </div>
