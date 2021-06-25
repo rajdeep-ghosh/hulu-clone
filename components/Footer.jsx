0c2aec14a18283eef0c08f72a88151db608d468e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-25 19:33:54 +0530

    Correct Github link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <Image src="/tmdb.svg" width={128} height={128} />
       <p className=" mb-2 text-lg font-medium">
         &copy; Rajdeep Ghosh | {" "} 
-        <a href="https://github.com/rajdeep-ghosh/" target="_blank">Github | </a>
+        <a href="https://github.com/rajdeep-ghosh/hulu-clone/" target="_blank">Github | </a>
         <a href="https://linkedin.com/in/rajdeepghosh01/" target="_blank">LinkedIn</a>
       </p>
     </div>
