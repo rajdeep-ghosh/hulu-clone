4f57fe4efd1714ab5451c4ac974fb0deea8e06db
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-09-05 10:27:59 +0530

    Change footer html element

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <div className="flex flex-col justify-center items-center p-2">
+    <footer className="flex flex-col justify-center items-center p-2">
       <Image src="/tmdb.svg" width={128} height={128} />
       <p className=" mb-2 text-lg font-medium">
         &copy; Rajdeep Ghosh | {" "} 
         <a href="https://github.com/rajdeep-ghosh/hulu-clone/" target="_blank">Github | </a>
         <a href="https://linkedin.com/in/rajdeepghosh01/" target="_blank">LinkedIn</a>
       </p>
-    </div>
+    </footer>
   );
 }
 
